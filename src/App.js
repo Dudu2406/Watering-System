0d9cf16c6c532aaafbe256b21927eb229975237b
Dudu2406 <bucalarobertangelo@gmail.com>
2024-11-19 23:43:44 +0800

    new code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "./views/login_page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </body>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/views/login_page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/main_view.css";
+import { Button } from "react-bootstrap";
+export const LoginPage = () =>{
+    return <>
+    <div className="backgroundColor w-100 ">
+        <Button className="bg-success text-capitalize">Hatdog</Button>
+        {/* <h1>a</h1>
+        <h1>a</h1>
+        <h1>a</h1> */}
+    </div>
+    </>
+}
