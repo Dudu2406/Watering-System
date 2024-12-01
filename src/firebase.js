dd16743714d73ec0d775f03661959135a8290ce0
Patricia Marie Bucala <85491599+PMB129@users.noreply.github.com>
2024-12-01 22:55:21 +0800

    hide keys

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANlKEq32C5XHytclczF6rU2GUFj6a5ePY",
-  authDomain: "wateringsystem-51008.firebaseapp.com",
-  projectId: "wateringsystem-51008",
-  storageBucket: "wateringsystem-51008.firebasestorage.app",
-  messagingSenderId: "377176174340",
-  appId: "1:377176174340:web:5a0650eb0188b89a726a51"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
